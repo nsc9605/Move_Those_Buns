a86fc5185bbf1161982c02582153afcd53395722
Niki Catapano <nsc9605@gmail.com>
2021-03-02 17:58:20 -0500

    image fix

--- models/Workout.js
+++ models/Workout.js
@@ -4,8 +4,7 @@ const Schema = mongoose.Schema;
 const workoutSchema = new Schema({
   day: {
     type: Date,
-    // default: new Date()
-    default: Date.now
+    default: () => Date.now()
   },
   exercises: [
     {
